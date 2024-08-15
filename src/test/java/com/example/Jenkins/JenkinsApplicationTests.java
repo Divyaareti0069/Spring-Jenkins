7e262568ab4df06dcdd5a91c987bf9815db516f2
Divyaareti0069 <48248294+Divyaareti0069@users.noreply.github.com>
2024-08-15 14:48:43 -0400

    second commit

--- src/test/java/com/example/Jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/Jenkins/JenkinsApplicationTests.java
@@ -1,18 +1,24 @@
 package com.example.Jenkins;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class JenkinsApplicationTests {
 
-	Logger logger= LoggerFactory.getLogger(JenkinsApplicationTests.class);
+	private static final Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
-		
+		logger.info("first test case");
+		logger.info("second test case");
+		assertEquals(true, true);
 	}
-
 }
